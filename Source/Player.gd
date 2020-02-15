e3782049e36198083f26cb1a4161af568c6bf67a
firststef <firstbossspike@gmail.com>
2020-02-15 22:03:24 +0200

    Finished initial test

--- Source/Player.gd
+++ Source/Player.gd
@@ -14,12 +14,13 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= MOUSE_SENS * event.relative.x
+		rotation_degrees.x -= MOUSE_SENS * event.relative.y
 
 func _process(delta):
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
 	if Input.is_action_pressed("restart"):
-		queue_free()
+		kill()
 
 func _physics_process(delta):
 	var move_vec = Vector3()
@@ -32,3 +33,15 @@ func _physics_process(delta):
 		move_vec.x -= 1
 	if Input.is_action_pressed("move_right"):
 		move_vec.x += 1
+	move_vec = move_vec.normalized()
+	move_vec = move_vec.rotated(Vector3(0,1,0), rotation.y)
+	move_and_collide(move_vec * MOVE_SPEED * delta)
+	
+	if Input.is_action_just_pressed("shoot") and ! anim_player.is_playing():
+		anim_player.play("ShootAnimation")
+		var coll = raycast.get_collider()
+		if raycast.is_colliding() and coll.has_method("kill"):
+			coll.kill()
+
+func kill():
+	get_tree().reload_current_scene()
